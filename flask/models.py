282899bb63b81d0779749a4eb8d656491287ef51
Charisios Tsiairis <chrstsrs@yahoo.gr>
2023-06-19 17:07:35 +0100

    update readme

--- flask/models.py
+++ flask/models.py
@@ -4,7 +4,7 @@ class ToyModel(db.Model):
     __tablename__ = "toys"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), unique=True, nullable=False)
+    name = db.Column(db.String(60), unique=True, nullable=False, index=True)
     description = db.Column(db.String(255))
     price = db.Column(db.Float(precision=2), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
